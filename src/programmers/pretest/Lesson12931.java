064e0a94fe40cc764ac004f8da33ad4ecedc18c5
gemdoq <gemdoq@gmail.com>
2022-10-11 09:49:40 +0900

    programmers자릿수더하기

---
+++ src/programmers/pretest/Lesson12931.java
@@ -0,0 +1,28 @@
+package programmers.pretest;
+
+import java.util.Scanner;
+
+public class Lesson12931 {
+    public int solution(int n) {
+        int answer = 0;
+
+        // for? while?
+        while( n > 0) {
+            answer += n%10;
+            n = n/10;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Lesson12931 ls = new Lesson12931();
+        int result = ls.solution(1234);
+
+        if(result == 10) {
+            System.out.println("test pass");
+        } else {
+            System.out.println("test fail");
+        }
+    }
+}
