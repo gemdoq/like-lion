1817f5cfbef0606f79b1dd64d0be9a92025083df
gemdoq <gemdoq@gmail.com>
2022-10-13 09:17:32 +0900

    polymorphism + linereader

--- src/week5/day2/line/parser/HospitalParser.java
+++ src/week5/day2/line/parser/HospitalParser.java
@@ -5,7 +5,7 @@ import week5.day2.line.domain.Hospital;
 public class HospitalParser implements Parser<Hospital>{
     @Override
     public Hospital parse(String str) {
-        String[] splitted = str.split(" ");
+        String[] splitted = str.split(",");
         return new Hospital(splitted[0]);
     }
 }
