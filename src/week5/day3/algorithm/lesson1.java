071da36814ad7c462d670d2be8d023a62578f4a6
gemdoq <gemdoq@gmail.com>
2022-10-13 10:16:42 +0900

    algorithm lesson1 2d for

--- src/week5/day3/algorithm/lesson1.java
+++ src/week5/day3/algorithm/lesson1.java
@@ -4,15 +4,23 @@ package week5.day3.algorithm;
 
 public class lesson1 {
     public int[] sort(int[] arr) {
-        for (int j = 0; j < arr.length; j++) {
-            System.out.println(arr[j]);
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
         }
         return arr;
     }
+
     public static void main(String[] args) {
         int[] arr = new int[]{7, 2, 3, 9, 28, 11};
 
         lesson1 ls1 = new lesson1();
-        ls1.sort(arr);
+        int[] result = ls1.sort(arr);
+        System.out.println(result);
     }
 }
