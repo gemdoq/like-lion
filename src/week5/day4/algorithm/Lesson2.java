8c4a36ccfe6b5f249cc3b98475c8df97156cd918
gemdoq <gemdoq@gmail.com>
2022-10-14 10:29:37 +0900

    insertion sort complete

--- src/week5/day4/algorithm/Lesson2.java
+++ src/week5/day4/algorithm/Lesson2.java
@@ -3,11 +3,16 @@ package week5.day4.algorithm;
 import java.util.Arrays;
 
 public class Lesson2 {
-    public int[] sort(int[] arr, int i) {
-        if(arr[i] < arr[i-1]) {
-            int pointer = arr[i];
-            arr[i] = arr[i-1];
-            arr[i-1] = pointer;
+    public int[] sort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+//                System.out.printf("i:%d j:%d j-1:%d\n", i, j, j - 1);
+                if (arr[j] < arr[j - 1]) { // if(condition) { swap; }
+                    int pointer = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = pointer;
+                }
+            }
         }
         return arr;
     }
@@ -15,7 +20,7 @@ public class Lesson2 {
     public static void main(String[] args) {
         int[] arr = {5,8,6,2,4};
         Lesson2 lesson2 = new Lesson2();
-        int[] result = lesson2.sort(arr, 2);
+        int[] result = lesson2.sort(arr);
 
         System.out.println(Arrays.toString(result));
     }
