626445963f08c3cf001e6ba90b720f3231f7b753
gemdoq <gemdoq@gmail.com>
2022-10-14 09:55:26 +0900

    insertion sort i=1

--- src/week5/day4/algorithm/Lesson1.java
+++ src/week5/day4/algorithm/Lesson1.java
@@ -6,9 +6,9 @@ public class Lesson1 {
     public int[] sort(int[] arr) {
         int i = 1;
         if(arr[i] < arr[i-1]) {
-            int temp = arr[i-1];
-            arr[i-1] = arr[i];
-            arr[i] = temp;
+            int pointer = arr[i];
+            arr[i] = arr[i-1];
+            arr[i-1] = pointer;
         }
         return arr;
     }
@@ -16,7 +16,6 @@ public class Lesson1 {
     public static void main(String[] args) {
         int[] arr = {8,5,6,2,4};
         Lesson1 lesson1 = new Lesson1();
-
         lesson1.sort(arr);
 
         System.out.println(Arrays.toString(arr));
