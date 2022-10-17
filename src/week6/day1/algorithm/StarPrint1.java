88a118e6ff1f7cfb71c7c72c75f605914088106f
gemdoq <gemdoq@gmail.com>
2022-10-17 09:14:50 +0900

    starprint1

---
+++ src/week6/day1/algorithm/StarPrint1.java
@@ -0,0 +1,10 @@
+package week6.day1.algorithm;
+
+public class StarPrint1 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
