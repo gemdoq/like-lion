87f987f0dc34a0e32d2877d21556ef7170548a62
gemdoq <gemdoq@gmail.com>
2022-10-17 09:32:04 +0900

    starprint1 + method + arg + constructor

--- src/week6/day1/algorithm/IncreasingTower.java
+++ src/week6/day1/algorithm/IncreasingTower.java
@@ -1,10 +1,15 @@
 package week6.day1.algorithm;
 
 public class IncreasingTower {
+    String s = "";
+    public IncreasingTower(String s) {
+        this.s = s;
+    }
+
     public void print(int n) {
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < i+1; j++) {
-                System.out.print("*");
+                System.out.print(s);
             }
             System.out.println();
         }
--- src/week6/day1/algorithm/StarPrinter.java
+++ src/week6/day1/algorithm/StarPrinter.java
@@ -2,7 +2,7 @@ package week6.day1.algorithm;
 
 public class StarPrinter {
     public static void main(String[] args) {
-        IncreasingTower increasingTower = new IncreasingTower();
+        IncreasingTower increasingTower = new IncreasingTower("s");
         increasingTower.print(3);
     }
 }
