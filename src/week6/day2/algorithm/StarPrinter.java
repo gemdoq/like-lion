e90ad793dba5658666c2a5b501c91d3a5f3b3520
gemdoq <gemdoq@gmail.com>
2022-10-18 10:02:18 +0900

    recursive startrinangle printer

--- src/week6/day2/algorithm/StarPrinter.java
+++ src/week6/day2/algorithm/StarPrinter.java
@@ -2,8 +2,11 @@ package week6.day2.algorithm;
 
 public class StarPrinter {
     public static void main(String[] args) {
-        Ractangle ractangle = new Ractangle(5,9,"*");
-        ractangle.print();
+        RecursiveStarTower recursiveStarTower = new RecursiveStarTower();
+        recursiveStarTower.print(5,1);
+
+//        Ractangle ractangle = new Ractangle(5,9,"*");
+//        ractangle.print();
 
 //        Square square = new Square(5, "*");
 //        square.print();
