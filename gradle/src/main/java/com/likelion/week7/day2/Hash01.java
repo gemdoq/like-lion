09126f651ff8cf2dc498111873e6d9f4ca5a4b16
gemdoq <gemdoq@gmail.com>
2022-10-25 10:08:50 +0900

    hashtable insert+search 1st

---
+++ gradle/src/main/java/com/likelion/week7/day2/Hash01.java
@@ -0,0 +1,64 @@
+package com.likelion.week7.day2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Hash01 {
+    /*
+    문제 설명
+    수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+
+    마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+
+    제한사항
+    마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다.
+    completion의 길이는 participant의 길이보다 1 작습니다.
+    참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
+    참가자 중에는 동명이인이 있을 수 있습니다.
+    */
+    private int size = 10000;
+    private int[] table = new int[size];
+
+    public Hash01() {
+    }
+
+    public Hash01(int size) {
+        this.size = size;
+        this.table = new int[size];
+    }
+
+    public int hash(String key){
+        int asciiSum = 0;
+        for(int i = 0; i < key.length(); i++){
+            asciiSum += key.charAt(i);
+        }
+        return asciiSum % size;
+    }
+
+    public void insert(String key, Integer value) {
+        int hashCode = hash(key);
+        this.table[hashCode] = value;
+        System.out.println(hashCode+"saved!");
+    }
+
+    public int search(String key) {
+        return this.table[hash(key)];
+    }
+
+    public static void main(String[] args) {
+        String[] names = new String[]{"DongyeonKang",
+                "SubinKang", "KwanwunKo", "HyunseokKo", "KyoungdukKoo", "YeonjiGu", "SoyeonKown", "OhsukKwon", "GunwooKim", "KiheonKim", "NayeongKim", "DohyeonKim", "MinkyoungKim", "MinjiKim", "SanghoKim", "SolbaeKim", "YejinKim", "EungjunKim", "JaegeunKim", "JeonghyeonKim", "JunhoKim", "JisuKim", "kimjinah", "HaneulKim", "HeejungKim", "KimoonPark", "EunbinPark", "JeongHoonPark", "JeminPark", "TaegeunPark", "JiwonBae", "SeunggeunBaek", "JihwanByeon", "HeungseopByeon", "JeongHeeSeo", "TaegeonSeo", "SeeYunSeok", "SuyeonSeong", "SeyoelSon", "MinjiSong", "JinwooSong", "hyunboSim", "SominAhn", "JiyoungAhn", "ChangbumAn", "SoonminEom",
+                "HyeongsangOh", "SuinWoo", "JuwanWoo", "InkyuYoon", "GahyunLee", "DaonLee", "DohyunLee", "SanghunLee", "SujinLee", "AjinLee", "YeonJae", "HyeonjuLee", "HakjunYim", "SeoyunJang", "SeohyeonJang", "JinseonJang", "SujinJeon", "SeunghwanJeon", "DaehwanJung", "JaeHyunJeung", "HeejunJeong", "GukhyeonCho", "MunjuJo", "YejiJo", "ChanminJu", "MinjunChoi", "SujeongChoi", "SeunghoChoi", "AyeongChoi", "GeonjooHan", "JinhyuckHeo", "MinwooHwang", "SieunHwang",
+                "JunhaHwang"};
+//        Hash01 hash01 = new Hash01();
+//        Set<Integer> nameSet = new HashSet<>();
+        Hash01 hash01 = new Hash01(200);
+        for (int i = 0; i < names.length; i++) {
+//            nameSet.add(hash01.hash(names[i]));
+            hash01.insert(names[i], hash01.hash(names[i]));
+        }
+        System.out.println(hash01.search("DongyeonKang"));
+        System.out.println(hash01.search("JiyoungAhn"));
+//        System.out.printf("%s %s",names.length, nameSet.size());
+    }
+}
