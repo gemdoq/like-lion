1da222e2f7111d3567561c6dee7439de9f52834e
gemdoq <gemdoq@gmail.com>
2022-10-28 09:34:39 +0900

    폰켓몬 1단계 문제 설계 입출력 설정

---
+++ gradle/src/main/java/com/likelion/week7/day5/Ponketmon.java
@@ -0,0 +1,27 @@
+package com.likelion.week7.day5;
+
+public class Ponketmon {
+
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        return answer;
+    }
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        int[] input = {};
+
+        Ponketmon ponketmon = new Ponketmon();
+        int answer = ponketmon.solution(input);
+
+        System.out.println(answer);
+    }
+
+}
