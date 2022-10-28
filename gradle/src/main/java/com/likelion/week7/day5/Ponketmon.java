1a1268615f5f0d58b7f5e8511a99e8e5190389e7
gemdoq <gemdoq@gmail.com>
2022-10-28 09:39:53 +0900

    폰켓몬 2단계 솔루션 로직 구상 및 작성

--- gradle/src/main/java/com/likelion/week7/day5/Ponketmon.java
+++ gradle/src/main/java/com/likelion/week7/day5/Ponketmon.java
@@ -1,18 +1,17 @@
 package com.likelion.week7.day5;
 
 public class Ponketmon {
-
+    //해쉬맵으로 키와 밸류 쌍 저장
+    //키는 포켓몬 종류배열 nums
+    //밸류는 포켓몬 갯수 getKey()
+    //반복문을 돌려서 nums에서 map에 키를 저장하면서 밸류는 1씩 증가
+    //이터레이터로 만들어서 가장 큰 밸류를 가진 값을 answer에 할당
+    //answer반환
     public int solution(int[] nums) {
         int answer = 0;
 
         return answer;
     }
-
-
-
-
-
-
 
 
     public static void main(String[] args) {
