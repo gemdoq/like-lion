a9bcd9a221971efd70e6d2e27f6a27966a1498f7
gemdoq <gemdoq@gmail.com>
2022-11-01 09:26:58 +0900

    소수판별 풀이1단계 입출력설정

---
+++ gradle/src/main/java/com/likelion/week8/day2/PrimeNumber.java
@@ -0,0 +1,21 @@
+package com.likelion.week8.day2;
+
+public class PrimeNumber {
+
+    /*
+    문제입출력설정
+     */
+
+    public boolean solution(int number) {
+        boolean answer = false;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int number = 13;
+
+        PrimeNumber primeNumber = new PrimeNumber();
+        System.out.printf("소수면 true, 아니면 false : %s", primeNumber.solution(number));
+    }
+}
