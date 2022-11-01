e4da21052e4d9c7cf917de596ecee32d47c8edf4
gemdoq <gemdoq@gmail.com>
2022-11-01 09:48:38 +0900

    소수판별 풀이3단계 솔루션 추가

--- gradle/src/main/java/com/likelion/week8/day2/PrimeNumber.java
+++ gradle/src/main/java/com/likelion/week8/day2/PrimeNumber.java
@@ -4,21 +4,30 @@ public class PrimeNumber {
 
     /*
     1번 방법 : 2부터 판별하는 수 전까지 나눠보고 나머지가 0이 안나온다면 소수
+    2번 방법 : 주어진 숫자 / 2를 소숫점 버린 숫자까지 나누어 보고 나머지가 0이 아니면 소수
      */
 
+    // 1번 방법
     public boolean solution(int number) {
         boolean answer = true;
 
-        for(int i=2; i<number; i++){
-            if(number % i == 0) answer = false;
-        }
+        for(int i = 2; i < number; i++) { if(number % i == 0) answer = false; }
+        return answer;
+    }
+
+    // 2번 방법
+    public boolean solution2(int number) {
+        boolean answer = true;
+
+        for(int i = 2; i < Math.floor(number/2); i++) { if(number % i == 0) answer = false; }
         return answer;
     }
 
     public static void main(String[] args) {
         int number = 13;
-
         PrimeNumber primeNumber = new PrimeNumber();
-        System.out.printf("주어진 수가 소수면 true, 아니면 false일 때, 주어진 수는 소수인가? 정답 : %s\n", primeNumber.solution(number));
+        boolean checked = primeNumber.solution2(number);
+
+        System.out.printf("주어진 수가 소수면 true, 아니면 false일 때, 주어진 수는 소수인가? 정답 : %s\n", checked);
     }
 }
