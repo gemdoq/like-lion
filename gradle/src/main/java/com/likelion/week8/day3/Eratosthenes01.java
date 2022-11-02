e5e7379d5aa1196f3410728c6ebe708918a4042d
gemdoq <gemdoq@gmail.com>
2022-11-02 09:48:23 +0900

    숫자리스트에서 2의 배수 제외하고 출력

--- gradle/src/main/java/com/likelion/week8/day3/Eratosthenes01.java
+++ gradle/src/main/java/com/likelion/week8/day3/Eratosthenes01.java
@@ -10,7 +10,7 @@ public class Eratosthenes01 {
         List<Integer> primeNumberList = new ArrayList<>();
         // 2~50 채우기
         for (int i = 2; i <= N; i++) primeNumberList.add(i);
-        // 2의 배수 지우기
+        // 2의 배수 지우기 ( removeIf()도 같은 역할 )
         for (int i = 0; i < primeNumberList.size(); i++) {
             if (primeNumberList.get(i) % 2 == 0 && primeNumberList.get(i) > 2) primeNumberList.remove(i);
         }
