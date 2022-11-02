d460480dfa4b5d35a7564878757691da82fc017d
gemdoq <gemdoq@gmail.com>
2022-11-02 10:34:39 +0900

    숫자리스트에서 정수 배수 제외하고 출력

--- gradle/src/main/java/com/likelion/week8/day3/Eratosthenes01.java
+++ gradle/src/main/java/com/likelion/week8/day3/Eratosthenes01.java
@@ -4,17 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Eratosthenes01 {
+    public List<Integer> solution(int maxNum) {
+        List<Integer> answer;
+
+        int N = maxNum;
+        List<Integer> primeNumberList = new ArrayList<>();
+
+        for (int i = 2; i <= N; i++) primeNumberList.add(i);
+
+        for (int j = 2; j * j <= N; j++) {
+            for (int i = 0; i < primeNumberList.size(); i++) {
+                if (primeNumberList.get(i) % j == 0 && primeNumberList.get(i) > j) primeNumberList.remove(i);
+            }
+        }
+        answer = primeNumberList;
+
+        return answer;
+    }
 
     public static void main(String[] args) {
-        int N = 50;
-        List<Integer> primeNumberList = new ArrayList<>();
-        // 2~50 채우기
-        for (int i = 2; i <= N; i++) primeNumberList.add(i);
-        // 2의 배수 지우기 ( removeIf()도 같은 역할 )
-        for (int i = 0; i < primeNumberList.size(); i++) {
-            if (primeNumberList.get(i) % 2 == 0 && primeNumberList.get(i) > 2) primeNumberList.remove(i);
-        }
-        System.out.println(primeNumberList);
-        System.out.println(primeNumberList.size());
+        Eratosthenes01 eratosthenes01 = new Eratosthenes01();
+        List<Integer> result = eratosthenes01.solution(10000);
+
+        System.out.println(result);
+        System.out.println(result.size());
     }
+
 }
