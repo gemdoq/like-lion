055a31c21170a20601cca3c569548dcce6ee3e62
gemdoq <gemdoq@gmail.com>
2022-11-05 23:14:17 +0900

    CodeUp1041

---
+++ gradle/src/main/java/com/likelion/week8/codeup/CodeUp1041.java
@@ -0,0 +1,13 @@
+package com.likelion.week8.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1041 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        int asciiNumOfInput = (int)input.charAt(0);
+        char nextChar = (char) (asciiNumOfInput+1);
+        System.out.printf("%s", nextChar);
+    }
+}
