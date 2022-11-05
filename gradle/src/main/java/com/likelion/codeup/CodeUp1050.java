b4cca09454af45f272eb067e88fa4af3633b7d96
gemdoq <gemdoq@gmail.com>
2022-11-06 01:11:56 +0900

    CodeUp1050

---
+++ gradle/src/main/java/com/likelion/codeup/CodeUp1050.java
@@ -0,0 +1,15 @@
+package com.likelion.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1050 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a==b) {
+            System.out.printf("1");
+        } else System.out.printf("0");
+    }
+}
