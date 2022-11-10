4493688c24574b6b020a9420c77d0f17fe13897e
gemdoq <gemdoq@gmail.com>
2022-11-10 09:15:47 +0900

    HateTheSameNumber 문제풀기 전 입출력설정

---
+++ gradle/src/main/java/com/likelion/week9/day4/HateTheSameNumber.java
@@ -0,0 +1,19 @@
+package com.likelion.week9.day4;
+
+public class HateTheSameNumber {
+    public int[] solution(int[] arr) {
+        int[] answer = {};
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        HateTheSameNumber hateTheSameNumber = new HateTheSameNumber();
+        int[] arr = {1, 1, 3, 3, 0, 1, 1};
+        int[] result = hateTheSameNumber.solution(arr);
+        System.out.println(result);
+    }
+}
