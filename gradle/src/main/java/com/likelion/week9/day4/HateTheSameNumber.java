fd379be977a47dcfef1138548004331e53eb0d6d
gemdoq <gemdoq@gmail.com>
2022-11-10 09:25:01 +0900

    HateTheSameNumber list로 풀이

--- gradle/src/main/java/com/likelion/week9/day4/HateTheSameNumber.java
+++ gradle/src/main/java/com/likelion/week9/day4/HateTheSameNumber.java
@@ -1,12 +1,23 @@
 package com.likelion.week9.day4;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class HateTheSameNumber {
     public int[] solution(int[] arr) {
         int[] answer = {};
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                list.add(arr[i]);
+            }
+        }
+        answer = new int[list.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
         return answer;
     }
 
@@ -14,6 +25,6 @@ public class HateTheSameNumber {
         HateTheSameNumber hateTheSameNumber = new HateTheSameNumber();
         int[] arr = {1, 1, 3, 3, 0, 1, 1};
         int[] result = hateTheSameNumber.solution(arr);
-        System.out.println(result);
+        System.out.println(Arrays.toString(result));
     }
 }
