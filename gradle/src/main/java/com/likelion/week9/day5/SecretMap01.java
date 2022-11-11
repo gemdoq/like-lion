db54b590b0ae1fd99014d5ec81332164cb2f1d41
gemdoq <gemdoq@gmail.com>
2022-11-11 09:13:04 +0900

    SecretMap01 입출력설정

---
+++ gradle/src/main/java/com/likelion/week9/day5/SecretMap01.java
@@ -0,0 +1,20 @@
+package com.likelion.week9.day5;
+
+import java.util.Arrays;
+
+public class SecretMap01 {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int[] arr1 = {9,20,28,18,11};
+        int[] arr2 = {30,1,21,17,28};
+        SecretMap01 secretMap01 = new SecretMap01();
+        String[] result = secretMap01.solution(n, arr1, arr2);
+        System.out.println(Arrays.toString(result));
+    }
+}
