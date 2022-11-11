bc8842fe96c0810a5d035d67a9656ed025a33b9b
gemdoq <gemdoq@gmail.com>
2022-11-11 09:22:22 +0900

    SecretMap01 수도코드작성

--- gradle/src/main/java/com/likelion/week9/day5/SecretMap01.java
+++ gradle/src/main/java/com/likelion/week9/day5/SecretMap01.java
@@ -5,7 +5,14 @@ import java.util.Arrays;
 public class SecretMap01 {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = {};
-
+        /*
+        배열의 각 요소를 2진법으로 변환
+        변환한 요소를 각각 배열로 변환해서 자릿수별로 자르기
+        자른 arr1의 배열요소와 같은 index의 arr2의 배열요소를 비교
+        둘 중 하나라도 1이 있을 경우 True고 아닐 경우 False
+        그 결과값을 true면 #으로, false면 공백으로 전환해서 연결
+        각 index를 돌며 배열에 담기
+         */
         return answer;
     }
 
