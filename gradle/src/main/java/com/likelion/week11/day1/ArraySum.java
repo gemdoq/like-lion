5417f66fef409731738c84e96799bf837c6dce81
gemdoq <gemdoq@gmail.com>
2022-11-22 10:44:42 +0900

    feature(ArraySum): recursive loop

--- gradle/src/main/java/com/likelion/week11/day1/ArraySum.java
+++ gradle/src/main/java/com/likelion/week11/day1/ArraySum.java
@@ -1,15 +1,15 @@
 package com.likelion.week11.day1;
 
 public class ArraySum {
-    static int sum(int[] arr, int answer, int index) {
-        if(arr.length == index) return answer;
+    static int sum(int[] arr, int index) {
+        if(arr.length == index) return 0;
         // idx + 1을 해서 탈출조건이 성립되게끔 만든다.
-        return sum(arr, answer + arr[index], index+1);
+        return arr[index] + sum(arr,index+1);
     }
 
     public static void main(String[] args) {
         int[] arr = new int[]{7,3,2,9};
-        int r = sum(arr, 0, 0);
+        int r = sum(arr,0);
         // 처음 호출할 때도 0을 추가로 넣어주어서 복잡해졌다.
         // 파라미터를 하나 더 추가해서 더 복잡해졌다.
         System.out.println(r);
