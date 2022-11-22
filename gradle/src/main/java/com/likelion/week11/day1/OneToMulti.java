364c96be088fcff00870b01a767cec59aa4d06be
gemdoq <gemdoq@gmail.com>
2022-11-22 09:36:04 +0900

    feature(OneToMulti): recursive loop

--- gradle/src/main/java/com/likelion/week11/day1/OneToMulti.java
+++ gradle/src/main/java/com/likelion/week11/day1/OneToMulti.java
@@ -1,12 +1,13 @@
 package com.likelion.week11.day1;
 
 public class OneToMulti {
-    public static void printOneToMulti(int start) {
+    public static void printOneToMulti(int start, int end) {
+        if(start > end) return;
         System.out.println(start);
-        printOneToMulti(start);
+        printOneToMulti(start+1, end);
     }
 
     public static void main(String[] args) {
-        OneToMulti.printOneToMulti(1);
+        OneToMulti.printOneToMulti(1, 100);
     }
 }
