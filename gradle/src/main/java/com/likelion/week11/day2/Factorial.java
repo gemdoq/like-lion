8003773406b5684b46270ab5c985eea3a95ee94d
gemdoq <gemdoq@gmail.com>
2022-11-23 09:54:12 +0900

    feature(Factorial): recursive loop

---
+++ gradle/src/main/java/com/likelion/week11/day2/Factorial.java
@@ -0,0 +1,13 @@
+package com.likelion.week11.day2;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Factorial factorial = new Factorial();
+        System.out.printf("%d\n", factorial.calc(5));
+    }
+
+    public int calc(int num) {
+        if (num == 1) return num;
+        return num * calc(num-1);
+    }
+}
