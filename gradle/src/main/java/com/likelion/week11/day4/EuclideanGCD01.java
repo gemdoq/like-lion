0853cd3189e27cdbedb0505aade9b6776a4d5d8e
gemdoq <gemdoq@gmail.com>
2022-11-25 09:28:42 +0900

    feature(EuclideanGCD01): Think up

---
+++ gradle/src/main/java/com/likelion/week11/day4/EuclideanGCD01.java
@@ -0,0 +1,15 @@
+package com.likelion.week11.day4;
+
+public class EuclideanGCD01 {
+    public static void main(String[] args) {
+        int a =1071;
+        int b = 1029;
+
+        int result = EuclideanGCD01.gcd(a, b);
+        System.out.printf("%d%n", result);
+    }
+
+    public static int gcd(int a, int b) {
+        return 0;
+    }
+}
