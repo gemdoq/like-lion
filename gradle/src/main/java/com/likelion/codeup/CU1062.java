e2f55d96deb9d2ea1823804b516c61abf7c2cd82
gemdoq <gemdoq@gmail.com>
2022-11-27 23:53:10 +0900

    feature(CU1062): solve

---
+++ gradle/src/main/java/com/likelion/codeup/CU1062.java
@@ -0,0 +1,12 @@
+package com.likelion.codeup;
+
+import java.util.Scanner;
+
+public class CU1062 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a ^ b);
+    }
+}
