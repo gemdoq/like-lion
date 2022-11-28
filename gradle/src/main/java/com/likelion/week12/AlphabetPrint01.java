3c520bb701fe060b3c0f0ee48fa76d09d54bf9d2
gemdoq <gemdoq@gmail.com>
2022-11-28 09:19:26 +0900

    feature(AlphabetPrint01): solve

--- gradle/src/main/java/com/likelion/week12/AlphabetPrint01.java
+++ gradle/src/main/java/com/likelion/week12/AlphabetPrint01.java
@@ -2,7 +2,9 @@ package com.likelion.week12;
 
 public class AlphabetPrint01 {
     public static void main(String[] args) {
-        AlphabetPrint01.print("A", "Z", 2);
+        char start = 'A';
+        char end = 'Z';
+        AlphabetPrint01.print(start, end, 2);
     }
 
     private static void print(char start, char end, int num) {
