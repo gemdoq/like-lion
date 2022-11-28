19e37b9a3c55abfc2669f43e2c065a41c930ab13
gemdoq <gemdoq@gmail.com>
2022-11-28 10:45:05 +0900

    fix(AlphabetPrint03): solve

--- gradle/src/main/java/com/likelion/week12/AlphabetPrint03.java
+++ gradle/src/main/java/com/likelion/week12/AlphabetPrint03.java
@@ -13,11 +13,13 @@ public class AlphabetPrint03 {
 
     private static void recursive(char start, char start2, char end) {
         if(start > end) return;
-        if(start2 > end) {
-            recursive(++start, start2, end);
+        if(start2 >= end) {
+            System.out.printf("%s%s%n", start, start2);
+            start2 = 'A';
+            recursive((char)(start+1), start2, end);
             return;
         }
         System.out.printf("%s%s%n", start, start2);
-        recursive(start, ++start2, end);
+        recursive(start, (char) (start2+1), end);
     }
 }
