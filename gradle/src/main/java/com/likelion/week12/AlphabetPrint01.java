dc5c8cb054277509d9fb84e2898a2368b591f9e6
gemdoq <gemdoq@gmail.com>
2022-11-28 10:03:02 +0900

    fix(01, 02): num delete

--- gradle/src/main/java/com/likelion/week12/AlphabetPrint01.java
+++ gradle/src/main/java/com/likelion/week12/AlphabetPrint01.java
@@ -4,10 +4,10 @@ public class AlphabetPrint01 {
     public static void main(String[] args) {
         char start = 'A';
         char end = 'Z';
-        AlphabetPrint01.print(start, end, 2);
+        AlphabetPrint01.print(start, end);
     }
 
-    private static void print(char start, char end, int num) {
+    private static void print(char start, char end) {
         for(char c = start; c <= end; c++) {
             System.out.println(c);
         }
--- gradle/src/main/java/com/likelion/week12/AlphabetPrint02.java
+++ gradle/src/main/java/com/likelion/week12/AlphabetPrint02.java
@@ -4,10 +4,10 @@ public class AlphabetPrint02 {
     public static void main(String[] args) {
         char start = 'A';
         char end = 'Z';
-        AlphabetPrint02.print(start, end, 2);
+        AlphabetPrint02.print(start, end);
     }
 
-    private static void print(char start, char end, int num) {
+    private static void print(char start, char end) {
         for(char c = start; c <= end; c++) {
             for(char c2 = start; c2 <= end; c2++) {
                 System.out.printf("%s%s%n", c, c2);
