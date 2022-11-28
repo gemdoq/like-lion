429e53f342608b12d650224ce2004b8dd7e34dbd
gemdoq <gemdoq@gmail.com>
2022-11-28 11:02:14 +0900

    feature(AlphabetPrint05): print partition combination

---
+++ gradle/src/main/java/com/likelion/week12/AlphabetPrint05.java
@@ -0,0 +1,18 @@
+package com.likelion.week12;
+
+public class AlphabetPrint05 {
+    public static final String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static void printAlphabet(String prefix, int depth) {
+        if (prefix.length() > depth) return;
+        System.out.println(prefix);
+
+        for (int i = 0; i < chars.length(); i++) {
+            printAlphabet(prefix + chars.charAt(i), depth);
+        }
+    }
+
+    public static void main(String[] args) {
+        printAlphabet("", 2);
+    }
+}
